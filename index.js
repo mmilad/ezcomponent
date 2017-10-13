8452e93119280da2dd15135b4245da9957a34df3
milad <milad@miladCmp.local>
2017-10-13 14:08:25 +0200

    updates

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+var fs = require('fs')
 var builder = require('./lib/build.js').run
 var args = process.argv.slice(2)
-console.log(args)
+if(args.length) {
+    if(args[0] === "watch") {
+        watchChanges()
+    }
+}
 builder()
+
+
+
+function watchChanges() {
+    fs.watch('./lib/jhcr.js', { encoding: 'buffer' }, (eventType, filename) => {
+        builder()
+    });
+}
